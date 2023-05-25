c6e72becf3e7159836aed8d771a100af90bb5455
AddisBelayhu <addisgithub@gmail.com>
2023-05-25 11:04:33 +0300

    user add page

--- user.php
+++ user.php
@@ -8,12 +8,35 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
 
-    <title>Hello, world!</title>
+    <title>crud operation</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
+    <div class="container my-5">
+    <form method = "POST">
+  <div class="form-group">
+    <label>Name</label>
+    <input type="text" class="form-control" placeholder="Enter your name" name="name" autocomplete = "off">
+  </div>
+ 
+  <div class="form-group">
+    <label>Email</label>
+    <input type="email" class="form-control" placeholder="Enter your email" name="email" autocomplete = "off">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-</body>
+  <div class="form-group">
+    <label>Mobile</label>
+    <input type="text" class="form-control" placeholder="Enter your mobile" name="mobile" autocomplete = "off">
+  </div>
+
+  <div class="form-group">
+    <label>Password</label>
+    <input type="password" class="form-control" placeholder="Enter your name" name="password" autocomplete = "off">
+  </div>
+
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+    </div>
+
+  </body>
 </html>
